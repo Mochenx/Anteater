f0103c15c335cc1d0741a1c0e533410866e5b0f4
mochenx <mochenx@gmail.com>
2014-12-25 00:45:16 +0800

    1. Added new methods from original draft scripts and some new test cases for them

--- Roles/Driver.py
+++ Roles/Driver.py
@@ -1,13 +1,23 @@
 # encoding=utf-8
 
-from Roles.Role import RoleCreatorWithLogger
+import re
+from datetime import datetime
+from lxml import etree
+from io import StringIO
+from six import with_metaclass
+import six.moves.urllib.parse as urlparse
+
+from Roles.Role import RoleCreatorWithLogger, Role, Logger
+from Roles.CAPTCHARecognizer import CAPTCHAError, CAPTCHARecognizer
 
 __author__ = 'mochenx'
 
 
-class Driver(object):
-    __metaclass__ = RoleCreatorWithLogger
-
+class Driver(with_metaclass(RoleCreatorWithLogger, Role, Logger)):
+    """
+        Driver is the class which takes the charge of login, though "user" would be the proper name usually.
+        However, current package is developed for booking drive lessons, Driver is a better name.
+    """
     def __init__(self, **kwargs):
         super(Driver, self).__init__(**kwargs)
 
@@ -25,3 +35,100 @@ class Driver(object):
             self.session = kwargs['session']
         else:
             raise ValueError('Properties drivername, password and session are needed for class Driver')
+
+    def login(self):
+        """
+        """
+        CAPTCHA_rdr = CAPTCHARecognizer(self.session, prefix_fname=self.drivername)
+
+        login_post_data = self._get_login_form()
+        for i in range(10):
+            try:
+                captcha = CAPTCHA_rdr.get_captcha()
+                self.debug(msg='CAPTCHA:{0}'.format(captcha), by='Login')
+                login_post_data.load_captcha(captcha)
+                self._post_login_form(login_post_data.urlencode(), True)
+            except CAPTCHAError as e:
+                self.debug(msg='CAPTCHA recognition error:{0}'.format(str(e)), by='Login')
+                self._get_net_text()
+                continue
+            except Exception as e:
+                raise LoginAgain(str(e))
+            break
+
+    def _get_login_form(self):
+        home_page_url = 'http://haijia.bjxueche.net/'
+
+        self.debug(msg='{0} at time {1}'.format(home_page_url, datetime.now()), by='get_form')
+        _, resp_body = self.session.open_url_n_read(url=home_page_url)
+
+        login_post_data = LoginForm(self.drivername, self.password)
+        # body is a UTF-8 encoded bytes-type variable, so decode it firstly
+        login_post_data.load_form_with_parsed_html(resp_body)
+
+        return login_post_data
+
+    def _post_login_form(self, data_being_posted):
+        login_url = 'http://haijia.bjxueche.net/'
+
+        self.debug(msg='{0} at time {1}'.format(data_being_posted, datetime.now()),
+                   by='post_login_data')
+        resp, body = self.session.post_with_response(url=login_url, data=bytes(data_being_posted), timeout=5)
+
+        self._is_captcha_error(body)
+        self.debug(msg='Post login data done at time {0}'.format(datetime.now()),
+                   by='post_login_data')
+
+    @staticmethod
+    def _is_captcha_error(resp_body):
+        re_captcha_error = re.compile(unicode(u"验证码错误了"), re.U)
+        tree = etree.parse(StringIO(resp_body.decode(encoding='utf-8')), etree.HTMLParser())
+
+        # Iterates all <input> tags
+        captcha_error = False
+        for u in tree.iterfind('.//script'):
+            if u.text is None:
+                continue
+            captcha_error = True if re_captcha_error.search(u.text) else captcha_error
+        if captcha_error:
+            raise CAPTCHAError('CAPTCHAError found after post')
+
+    def _get_net_text(self):
+        net_text_url = r'http://haijia.bjxueche.net/Login.aspx/GetNetText'
+
+        self.debug(msg='{0} at time {1}'.format(net_text_url, datetime.now()), by='get_net_text')
+        _, resp = self.session.open_url_n_read(url=net_text_url)
+
+
+class LoginAgain(BaseException):
+    pass
+
+
+class LoginForm(dict):
+    def __init__(self, drivername, password):
+        self['__VIEWSTATE'] = None
+        self['__EVENTVALIDATION'] = None
+        self['txtUserName'] = drivername
+        self['txtPassword'] = password
+        self['txtIMGCode'] = None
+        self['rcode'] = None
+
+    def load_form_with_parsed_html(self, resp_body):
+        tree = etree.parse(StringIO(resp_body.decode(encoding='utf-8')), etree.HTMLParser())
+        # Iterates all <input> tags
+        for u in tree.iterfind('.//input'):
+            name = u.get('id')
+            if name in self:
+                self[name] = u.get('value') if self[name] is None else self[name]
+
+    def load_captcha(self, captcha):
+        self['txtIMGCode'] = captcha
+        self['rcode'] = ''
+
+    def urlencode(self):
+        s_data_being_posted = urlparse.urlencode([(k, v) for k, v in self.items()])
+        s_data_being_posted += '&BtnLogin=%E7%99%BB++%E5%BD%95'
+        return s_data_being_posted
+
+    def __str__(self):
+        return '\n'.join(['{0}: {1}'.format(key, v) for key, v in self.items()])
---
+++ UTs/UTDriver.py
@@ -0,0 +1,55 @@
+# encoding=utf-8
+
+import unittest
+import httpretty
+
+from Roles.Driver import LoginForm, Driver, LoginAgain
+from Roles.Role import Role
+from Roles.Session import Session
+
+__author__ = 'mochenx'
+
+
+class UTDriver(unittest.TestCase):
+    def setUp(self):
+        self.shared_session = Session()
+        self.driver = Role.get('Driver')
+
+    def test_creation_n_properties_of_driver(self):
+        self.driver = Role.get('Driver')
+        self.assertTrue(hasattr(self.driver, 'drivername'))
+        self.assertTrue(hasattr(self.driver, 'password'))
+        self.assertTrue(hasattr(self.driver, 'session'))
+        self.assertEqual(self.driver.drivername, None)
+        self.assertEqual(self.driver.password, None)
+        self.assertEqual(self.driver.session, None)
+        self.driver.drivername = 'John Doe'
+        self.assertEqual(self.driver.drivername, 'John Doe')
+        print(self.driver.drivername)
+        print(self.driver.password)
+        print(self.driver.session)
+
+    def test_load_properties_success(self):
+        self.driver.load_properties(drivername='John Smith', password='888888', session=self.shared_session)
+        self.assertEqual(self.driver.drivername, 'John Smith')
+        self.assertEqual(self.driver.password, '888888')
+        self.assertIs(self.driver.session, self.shared_session)
+
+    def test_load_properties_fail(self):
+
+        def _no_drivername():
+            self.driver.load_properties(password='888888', session=self.shared_session)
+
+        def _no_password():
+            self.driver.load_properties(drivername='John Smith', session=self.shared_session)
+
+        def _no_session():
+            self.driver.load_properties(drivername='John Smith', password='888888')
+
+        self.assertRaises(ValueError, _no_drivername)
+        self.assertRaises(ValueError, _no_password)
+        self.assertRaises(ValueError, _no_session)
+
+    def test_get_login_form(self):
+        self.driver.load_properties(drivername='John Smith', password='888888', session=self.shared_session)
+        print(self.driver._get_login_form())
